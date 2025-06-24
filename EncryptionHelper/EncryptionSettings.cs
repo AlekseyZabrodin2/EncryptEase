507d7cbd946731bfb86e82b525bb85aa959f76fd
ADANI\zabrodin <rediskal50@gmail.com>
2025-06-24 11:09:57 +0300

    ref: add settings

---
+++ EncryptionHelper/EncryptionSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionHelper
+{
+    public class EncryptionSettings
+    {
+        public string? EncryptionKey { get; set; }
+        public string? EncryptionSalt { get; set; }
+    }
+}
